7dab2a91a70a41dcffc70989f55725429e5a8538
Arda Gündüz <91777992+ArdaGunduz00@users.noreply.github.com>
2021-12-19 18:23:50 +0300

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -2,49 +2,53 @@ import datetime
 
 print("Ardanın Özel Asistanı")
 
-tarih = datetime.datetime.now()
-kullaniciadi = input(("Kullanıcı Adı:"))
-kullanicişifre = input(("Şifre:"))
-
-if kullaniciadi == "arda" and kullanicişifre == "1234":
-    print("Hoşgeldiniz Efendim")
-
-else:
-    print("Yanlış Şifre")
-    quit()
-
-banasorusor = input(("Bana soru sorun efendim:")).lower()
-
-
 def banasorusor1(cevap):
     print(cevap)
 
-
-if banasorusor == ("adın ne senin"):
-    banasorusor1("Adım Jarvis")
-
-elif banasorusor == ("saat kaç"):
-    print(f'Şuan saat:{tarih}')
-
-elif banasorusor == ("nasılsın"):
-    banasorusor1("İyiyim")
-
-elif banasorusor == ("bana espri yaparmısın",):
-    banasorusor1("En güzel yemek yapan Ceren hangisidir? :TENCERE")
-
-elif banasorusor == ("görüşürüz",):
-    banasorusor1("Görüşürüz", )
-
-elif banasorusor == ("nerde yaşıyosun",):
-    banasorusor1("Ankara")
-
-elif banasorusor == ("ben nerde okuyorum",):
-    banasorusor1("Gölbaşı Anadolu Lisesinde")
-
-elif banasorusor == ("gelecekde nerce olacaksın"):
-    banasorusor1("Yanında")
+tarih = datetime.datetime.now()
+kullaniciadi = input("Kullanıcı Adı:")
+kullanicişifre = input("Şifre:")
+if kullaniciadi == "arda" and kullanicişifre == "1234":
+       print("Hoşgeldiniz Efendim")
 
 else:
-    banasorusor1("Beni daha buna kodlamadın :")
+       print("Yanlış Şifre")
+       quit()
+while True:
+       banasorusor = input(("Bana soru sorun efendim:")).lower()
+       if banasorusor == ("adın ne senin"):
+           banasorusor1("Adım Jarvis")
+
+       elif banasorusor == ("saat kaç"):
+           print(f'Şuan saat:{tarih}')
+
+       elif banasorusor == ("nasılsın"):
+           banasorusor1("İyiyim")
+
+       elif banasorusor == ("bana espri yaparmısın",):
+           banasorusor1("En güzel yemek yapan Ceren hangisidir? :TENCERE")
+
+       elif banasorusor == ("görüşürüz",):
+           banasorusor1("Görüşürüz", )
+
+       elif banasorusor == ("nerde yaşıyosun",):
+           banasorusor1("Ankara")
+
+       elif banasorusor == ("ben nerde okuyorum",):
+           banasorusor1("Gölbaşı Anadolu Lisesinde")
+
+       elif banasorusor == ("gelecekde nerde olacaksın"):
+           banasorusor1("Yanında")
+       elif banasorusor=="çıkmak istiyorum":
+           banasorusor1("Çıkılıyor")
+           break
+       else:
+           banasorusor1("Beni daha buna kodlamadın :")
+
+
+
+
+
+
 #Gelişim aşamasında Döngüler Fonksiyonlar ve Makine Öğreninimi ile geri dönecek...
 #Arda Gündüz
