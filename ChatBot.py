547b2cdda2e5381ecf0087ae09a74fb338da75a6
Arda Gündüz <91777992+ArdaGunduz00@users.noreply.github.com>
2022-01-11 20:46:14 +0300

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,54 +1,82 @@
 import datetime
-
-print("Ardanın Özel Asistanı")
-
-def banasorusor1(cevap):
-    print(cevap)
-
+import pygame as py
+import os
+import webbrowser
+from turtle import Turtle, Screen
 tarih = datetime.datetime.now()
-kullaniciadi = input("Kullanıcı Adı:")
-kullanicişifre = input("Şifre:")
-if kullaniciadi == "arda" and kullanicişifre == "1234":
-       print("Hoşgeldiniz Efendim")
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+print("""
+Robot İle Konuş""")
 
+def voicerobot(answer):
+    py.mixer.init()
+    py.mixer.music.load(answer)
+    py.mixer.music.play()
+
+
+useradd = input("Kullanıcı Adı:")
+userpassword = input("Şifre:")
+if useradd == "arda" and userpassword == "1234":
+    voicerobot("sir.mp3")
 else:
-       print("Yanlış Şifre")
-       quit()
-while True:
-       banasorusor = input(("Bana soru sorun efendim:")).lower()
-       if banasorusor == ("adın ne senin"):
-           banasorusor1("Adım Jarvis")
+    print("Yanlış Şifre ya da Kullanıcı adı")
+    quit()
+try:
 
-       elif banasorusor == ("saat kaç"):
+    while True:
+       question = input(("Bana soru sorun efendim:")).lower()
+       if question == ("adın ne senin"):
+           voicerobot("Ad.Jarvis.mp3")
+
+       elif question == ("saat kaç"):
            print(f'Şuan saat:{tarih}')
 
-       elif banasorusor == ("nasılsın"):
-           banasorusor1("İyiyim")
+       elif question == ("nasılsın"):
+           voicerobot("iyiyim.mp3")
 
-       elif banasorusor == ("bana espri yaparmısın",):
-           banasorusor1("En güzel yemek yapan Ceren hangisidir? :TENCERE")
+       elif question == ("bana espri yaparmısın"):
+           voicerobot("espri.mp3")
 
-       elif banasorusor == ("görüşürüz",):
-           banasorusor1("Görüşürüz", )
+       elif question == ("görüşürüz"):
+           voicerobot("Görüşürüz" )
 
-       elif banasorusor == ("nerde yaşıyosun",):
-           banasorusor1("Ankara")
+       elif question == ("nerde yaşıyosun"):
+           voicerobot("nerdeyasıon.mp3")
 
-       elif banasorusor == ("ben nerde okuyorum",):
-           banasorusor1("Gölbaşı Anadolu Lisesinde")
+       elif question == ("ben nerde okuyorum"):
+           voicerobot("okul.mp3")
 
-       elif banasorusor == ("gelecekde nerde olacaksın"):
-           banasorusor1("Yanında")
-       elif banasorusor=="çıkmak istiyorum":
-           banasorusor1("Çıkılıyor")
+       elif question == ("gelecekde nerde olacaksın"):
+           voicerobot("gelecekte nerde olacaksın.mp3")
+
+       elif question=="çıkmak istiyorum":
+           voicerobot("çıkış..mp3")
+       elif question=="bana şarkı çal":
+           webbrowser.get(chrome_path).open('https://www.youtube.com/watch?v=6xJYQGRHM7c/')
+       elif question=="bana resim çiz":
+           wn = Screen()
+           def tree(length, t):
+               if length > 5:
+                   t.forward(length)
+                   t.right(40)
+                   tree(length - 15, t)
+                   t.left(80)  # sola dön
+                   tree(length - 15, t)
+                   t.right(40)
+                   t.backward(length)
+
+
+           t = Turtle()
+           t.left(90)
+           t.speed(0)
+           tree(100, t)
+           wn.exitonclick()
+
+
            break
-       else:
-           banasorusor1("Beni daha buna kodlamadın :")
 
+    else:
+           voicerobot("Beni daha buna kodlamadın :")
 
-
-
-
-
-#Gelişim aşamasında Döngüler Fonksiyonlar ve Makine Öğreninimi ile geri dönecek...
-#Arda Gündüz
+except:
+    print(f'{question} sorusuna cevap bulununamadı geliştiricim bunu ekleyecektir')
