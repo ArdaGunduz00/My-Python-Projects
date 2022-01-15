5762fb5550af3052b020ecb872bfe028931515a5
Arda Gündüz <91777992+ArdaGunduz00@users.noreply.github.com>
2022-01-15 11:56:37 +0300

    Add files via upload

---
+++ Aylık Gider Gelir Hesaplayıcı_Beta.py
@@ -0,0 +1,25 @@
+print("Aylık Gider Gelir Hesaplayıcı")
+while True:
+    aylar=[]
+    month=input("Ay giriniz:")
+    if month=="q":
+        break
+    aylar.append(month)
+    try:
+        salary=int(input("Maaş giriniz:"))
+        extra_salary=int(input("Ekstra maaş giriniz:"))
+        electric=int(input("Elektirik faturası giriniz:"))
+        naturalgas=int(input("Doğal gaz faturası giriniz:"))
+        rent=int(input("Kira giriniz"))
+        shoppping_expensives=int(input("Market Alışverişinizi giriniz:"))
+        bank_debt=int(input("Banka borcunuzu giriniz:"))
+        revennue=salary+extra_salary
+        expense=electric+naturalgas+rent+shoppping_expensives+bank_debt
+        result=revennue-expense
+    except ValueError:
+        print("Sayısal Bir değer giriniz")
+        break
+
+    print(f'{month} ayında elinizde kalan para ya da kalmayan para {result} ')
+
+
